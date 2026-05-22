15e25aff3e50fd398e4c90f3844939fd187173a4
Kevin Crandall <kevin.crandall@snhu.edu>
2026-05-22 19:51:45 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+
+/* Read trips JSON data */
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel page */
 var travel = (req, res) => {
-    res.render('travel', { title: 'Travel' });
+    res.render('travel', {
+        title: 'Travel',
+        trips
+    });
 };
 
 module.exports = {
